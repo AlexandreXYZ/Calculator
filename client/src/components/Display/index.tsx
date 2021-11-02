2638e00a4a05b00b973a2061c080ab3c626206db
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-11-02 12:11:51 -0300

    Add display value and Reset Feature

--- client/src/components/Display/index.tsx
+++ client/src/components/Display/index.tsx
@@ -1,16 +1,21 @@
+import { useEffect, useState } from 'react';
 import style from './style.module.scss';
 
 interface props {
+  value?: string | number,
   className?: string | CSSModuleClasses
 }
 
-export const Display = ({className = style.display}: props ) => {
-
-  console.log({className})
+export const Display = ({value = '', className = style.display}: props ) => {
+  const [calc, setCalc] = useState(value) 
+  
+  useEffect(()=>{
+    setCalc(value);
+  })
 
   return(
     <div className={`${className}`}>
-
+      <output onChange={ ()=> calc} className={`${style.display_output}`}>{calc}</output>
     </div>
   )
 }
